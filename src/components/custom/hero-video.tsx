a39cbe7c2d67a89a6174efe9745167d7317702fc
codewithtabish <tabish169000@gmail.com>
2025-06-17 21:24:38 -1200

    blog banner is in progress

--- src/components/custom/hero-video.tsx
+++ src/components/custom/hero-video.tsx
@@ -10,8 +10,10 @@ export default function HeroVideo() {
       <HeroVideoDialog
         className="block dark:hidden w-full aspect-video rounded-xl shadow-md overflow-hidden"
         animationStyle="top-in-bottom-out"
-        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-        thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
+        videoSrc="https://5bl4nawh55.ufs.sh/f/aETJ5rHKEzpCLul3SmruNlGpzw2r6myM3n5VeY14sgFH8chR"
+        // videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+        thumbnailSrc="https://5bl4nawh55.ufs.sh/f/aETJ5rHKEzpCgFmmwV1kwb2pQ51rzEMsL8PjH9XNi6ngqKoa"
+        // thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
         thumbnailAlt="Hero Video"
       />
 
@@ -19,8 +21,10 @@ export default function HeroVideo() {
       <HeroVideoDialog
         className="hidden dark:block w-full aspect-video rounded-xl shadow-md overflow-hidden"
         animationStyle="top-in-bottom-out"
-        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-        thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
+        videoSrc="https://5bl4nawh55.ufs.sh/f/aETJ5rHKEzpC8GrE76vdiUqeJDvMn8S0PaLEHulzAxIco9rs"
+        // videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+        thumbnailSrc="https://5bl4nawh55.ufs.sh/f/aETJ5rHKEzpClnj0TmW3iFw05BH9N7M4rdogzV2CGZce1ERx"
+        // thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
         thumbnailAlt="Hero Video"
       />
     </div>
